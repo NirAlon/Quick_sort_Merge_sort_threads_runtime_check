5d384a0b2657f308e9a43db53795e3c1bb19fc46
Mavgi <avgimatan@gmail.com>
2020-05-13 18:04:43 +0300

    add greater equal implantation in animal

--- animal.py
+++ animal.py
@@ -7,7 +7,7 @@ class Animal:
         self.num_of_legs = num_of_legs  # 0 - 10
         self.tail = tail  # true/false
 
-    def __le__(self, other):
+    def __le__(self, other):  # <=
         if self.height < other.height:
             return True
         elif self.height > other.height:
@@ -25,7 +25,7 @@ class Animal:
                 else:  # equals, checking num of legs
                     return self.num_of_legs < other.num_of_legs
 
-    def __lt__(self, other):
+    def __lt__(self, other):  # <
         if self.height < other.height:
             return True
         elif self.height > other.height:
@@ -42,6 +42,24 @@ class Animal:
                     return False
                 else:  # equals, checking num of legs
                     return self.num_of_legs < other.num_of_legs
+
+    def __ge__(self, other):  # >=
+        if self.height > other.height:
+            return True
+        elif self.height < other.height:
+            return False
+        else:  # equals, checking weight
+            if self.weight > other.weight:
+                return True
+            elif self.weight < other.weight:
+                return False
+            else:  # equals, checking age
+                if self.age > other.age:
+                    return True
+                elif self.age < other.age:
+                    return False
+                else:  # equals, checking num of legs
+                    return self.num_of_legs > other.num_of_legs
 
     def __repr__(self):
         return '(height: {0}, weight: {1}, age: {2}, num_of_legs: {3}, tail: {4})'.\
