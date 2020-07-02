45b7ac57a163b27a320dbb892f7eecac1326d7c6
Matan Avgi <avgimatan@gmail.com>
2020-07-02 12:29:56 +0300

    Delete config.py

--- #305356198#204456925/config.py
+++
@@ -1,8 +0,0 @@
-
-ARRAY_LENGTH = 10000000
-
-# a for ascending  עולה
-# d for descending  יורד
-SORT_ORDER = 'a'
-
-list = ['num_of_legs','age','weight','height'] #Order the list by priority, The first element is the first priority
